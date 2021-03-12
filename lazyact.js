1a8de00c8ccefb5f9d8130ff0622b29a098affc3
CountTo25 <fiammathegreat@ya.ru>
2021-03-12 04:16:54 +0300

    repeatable object, demo
    also repeatable elements and better documentation with demo

--- lazyact.js
+++ lazyact.js
@@ -3,8 +3,9 @@ class Lazyact {
   	this.dataSourcer = referencer;
     this.source = referencer();
     this.interval = setInterval(
-        function () {this.data = this.dataSourcer();}.bind(this),
-        refreshRate);
+      function () {this.data = this.dataSourcer();}.bind(this),
+      refreshRate
+    );
 
     this.updateHooks = {
       tick: {
@@ -14,29 +15,30 @@ class Lazyact {
     }
 
     this.setupFillers();
+    this.setupRepeaters();
 
     this.updateHooks.tick.worker = setInterval(
-     function() {this.updateHooks.tick.fireable.forEach(function(m){m();}.bind(this))}.bind(this),
+     this.tick.bind(this),
      refreshRate
    );
+   this.tick();
+  }
 
-
+  tick() {
+    this.updateHooks.tick.fireable.forEach(function(m){m();})
   }
 
   setupFillers() {
-    console.log('Binding UI to data...');
     let all = document.querySelectorAll('[bind]:not([repeat])');
     all.forEach(function(element) {
-      console.log(element);
       let source = element.getAttribute('source');
       source = source.replace('()', '');
-      let on = element.getAttribute('on');
+      let on = element.hasAttribute('on') ? element.getAttribute('on') : 'tick';
       let changer = null;
       changer = function() {
-        let erroredOut = false;
         let chain = source.split('.');
         let pointer = this.source;
-        let path = '';
+        let path = 'source';
         let prev = this.source;
         chain.forEach((part) => {
             if (part in pointer) {
@@ -45,13 +47,9 @@ class Lazyact {
                 path+='.'+part;
                 return;
             }
-            if (!erroredOut) {
-                throw new Error('Cant find '+part+' in '+pointer.name+' @ '+path);
-                erroredOut = true;
-            }
         });
 
-        if (pointer !== null) {
+        if (pointer !== this.source) {
             let text = pointer.toString();
             if (element.getAttribute('source').includes('()')) {
                 let toCall = pointer.bind(prev);
@@ -63,18 +61,115 @@ class Lazyact {
                 text = element.getAttribute('template').replace('%value%', text);
             }
 
-            if (element.getAttribute('to') === 'html')
+            if (element.getAttribute('to') === 'html' || !element.hasAttribute('to'))
                 element.innerHTML = text;
-            if (element.getAttribute('to') === 'style')
-                element.setAttribute('style', text);
-
+            element.setAttribute(element.getAttribute('to'), text);
         }
-
-    }
+      }
     this.updateHooks[on].fireable.push(changer.bind(this));
-}.bind(this));
+    }.bind(this));
   }
+
   setupRepeaters() {
-    
+    let repeaters = document.querySelectorAll('[iterate]');
+    repeaters.forEach(function(repeater) {
+        let _virtual = repeater.cloneNode(true);
+        let datasource = this.source;
+
+        let on = repeater.hasAttribute('on') ? repeater.getAttribute('on') : 'tick';
+        let source = repeater.getAttribute('iterate').replace('()', '');
+
+        let changer = null;
+
+        changer = function() {
+            let chain = source.split('.');
+            let pointer = this.source;
+            let path = 'source';
+            let prev = this.source;
+
+            chain.forEach((part) => {
+                if (part in pointer) {
+                    prev = pointer;
+                    pointer = pointer[part];
+                    path+='.'+part;
+                    return;
+                }
+            });
+
+            if (pointer !== this.source) {
+                let repeat = pointer;
+                if (repeater.getAttribute('iterate').includes('()')) {
+                    let toCall = pointer.bind(prev);
+                    repeat = toCall();
+                }
+
+                if (repeater.hasAttribute('object')) {
+                    for (const [key, value] of Object.entries(repeat)) {
+                        let workWith = null;
+                        let source = '';
+                        if (repeater.querySelector('[repeatable][repeated="'+key+'"]')) {
+                            workWith = repeater.querySelector('[repeatable][repeated="'+key+'"]')
+                        }
+
+                        if (!workWith) {
+                            workWith = repeater.querySelector('[repeatable]:not([repeated])').cloneNode(true);
+                            repeater.appendChild(workWith);
+                            workWith.setAttribute('repeated', key);
+                        }
+                        workWith.querySelectorAll('[bind]').forEach(function(bindable) {
+                            if (!bindable.hasAttribute('bind')) {
+                                return;
+                            }
+                            let chn = bindable.getAttribute('source').replace('repeatable.', '').split('.');
+                            let pnt = value;
+                            let pth = 'repeatable';
+                            let prv = value
+                            chn.forEach(part => {
+                                if (part in pnt) {
+                                    prv = pnt;
+                                    pnt = pnt[part];
+                                    pth+='.'+part;
+                                    return;
+                                }
+                            })
+
+                            if (pnt !== value) {
+                                let txt = pnt.toString();
+                                if (bindable.getAttribute('source').includes('()')) {
+                                    let toCall = pnt.bind(prev);
+                                    txt = toCall();
+                                }
+
+                                if (bindable.hasAttribute('template'))
+                                {
+                                    txt = bindable.getAttribute('template').replace('%value%', txt);
+                                }
+                                if (bindable.getAttribute('to') === 'html' || !bindable.hasAttribute('to'))
+                                    bindable.innerHTML = txt;
+                                bindable.setAttribute(bindable.getAttribute('to'), txt);
+                            }
+                        })
+                    }
+                }
+
+                if (repeater.hasAttribute('array')) {
+
+                }
+                /*
+                if (repeater.hasAttribute('template'))
+                {
+                    text = repeater.getAttribute('template').replace('%value%', text);
+                }
+
+                if (repeater.getAttribute('to') === 'html' || !repeater.hasAttribute('to'))
+                  repeater.innerHtml = text;
+
+                if (repeater.getAttribute('to') === 'style')
+                  repeater.setAttribute('style', text);
+                */
+            }
+        }
+        this.updateHooks[on].fireable.push(changer.bind(this));
+    }.bind(this));
   }
 }
--- readme.md
+++ readme.md
@@ -1,14 +1,102 @@
+# Lazyact
 This is stupidest reactive framework that I made in my free time
 
+Idea is not having to bother with components or whatever, just add some attributes to your html and have fun having reactive UI
+#### How to use
+>check [github pages](https://countto25.github.io/Lazyact/) of demo and [its source](https://github.com/CountTo25/Lazyact/blob/main/index.html) to see the goods in the effect
 
-Basic usage
-```javascript
-let _obj = {
-	counter: 0,
-}
-
-let lz = new Lazyact(function(){return _obj}, 1000);
-
-setInterval(() => {_obj.counter++}, 50);
-
+Grab lazyact.js from repo
+```html
+<script src='lazyact.js'><script>
 ```
+Tag your HTML elements with info on how to populate it
+
+```html
+<div
+	bind
+    source='pagename'
+></div>
+<div
+	bind
+    to='html'
+    source='randomMessages.hello()'
+></div>
+<div
+	bind
+    on='tick'
+    to='html'
+    template='Time spent watching timer: %value%'
+    source='timeSpent'
+></div>
+```
+
+declare your object or get it from whereever
+```javascript
+let uiData = {
+	pagename: 'Super Serious Company LLC Homepage',
+    timeSpent: 0,
+    teamMembers: {
+    	me: 'CountTo25',
+        myCat: 'Captain',
+    },
+    randomMessages: {
+    	hello: () => {
+        	let a = ['hi', 'hello', 'hey', 'heyah', 'sup'];
+            return a[~~(Math.random()*(a.length-1))];
+        }
+    }
+}
+```
+Create referencer function
+```
+let callMeWhatever = function() {return uiData}
+```
+Create new Lazyact using referencer and base interval timer
+```javascript
+let lz = new Lazyact(callMeWhatever, 1000);
+```
+freely modify your source object
+```javascript
+setInterval(()=>{uiData.timeSpent++}, 1000);
+```
+
+???
+
+Enjoy lazy reactive stuff!
+
+## More stuff
+
+#### Bind data to attributes other than html
+object that is subject to be lazyactified (wow!) can implement attribute **to** that tells which attribute to bind to. If no **to** specified, it will be bound to html. Or if you've decided to keep **to='html'**
+```html
+<div bind to='style' source='color()' template='color: %value%'>I'm changing colors!</div>
+```
+```javascript
+uiData.color = ()=>{return '#'+Math.floor(Math.random()*16777215).toString(16)};
+```
+#### Repeaters
+You can iterate trough an object (array coming rather soon, i just wanna sleep and work and then work on this)
+
+```html
+<div
+	iterate="teamMembers" object
+>
+  <span>Made by: <span>
+  <span repeatable>
+    <span
+      bind repeat
+      to="html"
+      source="repeatable.knownas"
+     ></span>
+     <span
+       bind repeat
+       to="html"
+       source="repeatable.tagline"
+       template = " — %value%"
+     ></span>
+  </span>
+  </div>
+```
+
+## TODO
+Arrays, more documentation
